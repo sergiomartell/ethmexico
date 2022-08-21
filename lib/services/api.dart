647f598db1908c413e7703a7a6b00c5f432312f4
Adrian Gutiérrez <adriangutierrez@MacBook-Pro-de-Adrian-2.local>
2022-08-20 21:21:45 -0500

    obtener perfiles

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,7 +1,8 @@
 import 'package:graphql/client.dart';
-import '../models/models.dart'
+import '../models/models.dart';
 
 class ApiServices {
+  //función para obtener los perfiles con más seguidores
   void fetchResults() async {
   final _httpLink = HttpLink(
     'https://api-mumbai.lens.dev/',
@@ -10,7 +11,7 @@ class ApiServices {
   /// **NOTE** The default store is the InMemoryStore, which does NOT persist to disk
   cache: GraphQLCache(),
   link: _httpLink,
-);
+  );
 
   const String readRepositories = r'''
     query ExploreProfiles {
@@ -109,21 +110,43 @@ class ApiServices {
     document: gql(readRepositories)
   );
   final QueryResult result = await client.query(options);
-    print(result.data[]);
+    print(result.data);
 
-    LensApi lens = LensApi.fromJson(result.data);
+    //LensApi lens = LensApi.fromJson(result.data);
     if (result.hasException) {
     print(result.exception.toString());
   }
 
   }
-  void like() async {
-    
-  }
-  
-  
 
+ 
+  void like() async {
+    final _httpLink = HttpLink(
+    'https://api-mumbai.lens.dev/',
+    );
+    final GraphQLClient client = GraphQLClient(
+    /// **NOTE** The default store is the InMemoryStore, which does NOT persist to disk
+    cache: GraphQLCache(),
+    link: _httpLink,
+    );
+    const String addReaction = r'''
+      mutation($request: ReactionRequest!) {
+      addReaction(request: $request)
+     }
+    ''';
+
+    final QueryOptions options = QueryOptions(
+      document: gql(addReaction)
+    );
+    final QueryResult result = await client.query(options);
+    print(result.data);
+
+    mutation AddReaction {
+    addReaction(request: { profileId: "0xe398", reaction: "UPVOTE", publicationId: "0xe398-0x02" })
+    }
+}
+
+}
   //final List<dynamic> repositories =
       //result.data['viewer']['repositories']['nodes'] as List<dynamic>;
-
 }
